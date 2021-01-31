24afd8f0744c7fb08dc556a898b125e95e16a547
Bruce Liu <lightliudark@gmail.com>
2021-01-31 01:49:21 -0700

    motor control functions preparation

---
+++ drone_motor_control.ino
@@ -0,0 +1,31 @@
+#include <servo.h>
+int motor_max = 2000;
+int motor_inital = 1000;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void test_motor_control()
+{
+  
+
+
+  
+}
+
+void motor_auto_calibrate()
+{
+
+  for(int motor_intial = 0; motor_inital <= motor_max; motor_inital += 100)
+  {
+  }
+
+  
+}
